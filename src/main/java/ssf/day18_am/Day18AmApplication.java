fe0050ade5bf929865d82e8b870989b8c0c3d7e8
Beatrice <beatricesim31@gmail.com>
2024-12-02 09:47:31 +0800

    spring-template

---
+++ src/main/java/ssf/day18_am/Day18AmApplication.java
@@ -0,0 +1,13 @@
+package ssf.day18_am;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day18AmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day18AmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssf/day18_am/Day18AmApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.day18_am;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day18AmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
