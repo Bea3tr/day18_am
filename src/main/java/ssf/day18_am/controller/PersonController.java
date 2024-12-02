5668dd769237946c2b0ec4e002ce1f749a43d9ac
Beatrice <beatricesim31@gmail.com>
2024-12-02 16:39:08 +0800

    try request body

--- src/main/java/ssf/day18_am/controller/PersonController.java
+++ src/main/java/ssf/day18_am/controller/PersonController.java
@@ -3,10 +3,12 @@ package ssf.day18_am.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.validation.Valid;
@@ -24,6 +26,15 @@ public class PersonController {
     public String getIndex(Model model) {
         model.addAttribute("person", new Person());
         return "person";
+    }
+
+    @PostMapping("/create")
+    public String postMethodName(@Valid @RequestBody MultiValueMap<String, String> form, BindingResult bindings) {
+        Person person = new Person(form.getFirst("id"), form.getFirst("fullName"), 
+            form.getFirst("email"), form.getFirst("postalCode"), form.getFirst("phoneNumber"));
+        perSvc.insertPerson(person);
+
+        return "redirect:/person/listing";
     }
     
     @PostMapping
